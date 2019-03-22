4d2f6fcf4e5c7426ed8cdd558583f97d4a63c76f
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-22 22:02:17 +0000

    using query string package in posts.jsx

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,6 +1,11 @@
 import React from "react";
+import queryString from 'react-scripts';
 
-const Posts = ({match}) => {
+const Posts = ({ match, location }) => {
+
+  const { sortBy } = queryString.parse(location.search);
+  console.log(sortBy);
+
   return (
     <div>
       <h1>Posts</h1>
